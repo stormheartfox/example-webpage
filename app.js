17f8dfa447413796084dfbf4888731333ccbc133
michaeljgrant <mjg.grant.m@gmail.com>
2024-08-02 19:43:44 +1000

    First example

---
+++ app.js
@@ -0,0 +1,5 @@
+const paragraphWithId = document.getElementById("first-paragraph");
+
+paragraphWithId.innerText(
+  "Tom's mother is a fine cook and hopefully she makes us a really nice pie one day"
+);
