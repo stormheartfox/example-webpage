0c64851ae005355d730afb1cd94d15a3383bd778
michaeljgrant <mjg.grant.m@gmail.com>
2024-08-02 20:21:41 +1000

    add example event listeners

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const paragraphWithId = document.getElementById("first-paragraph");
+const clickButton = document.getElementById("first-button");
+const clickButton2 = document.getElementById("another-button");
+const clickADiv = document.getElementById("myClickableDiv");
 
-paragraphWithId.innerText(
-  "Tom's mother is a fine cook and hopefully she makes us a really nice pie one day"
-);
+const changeItBack = () => {
+  paragraphWithId.innerText =
+    "Tom's mother is a fine cook and hopefully she makes us a really nice pie one day";
+};
+const changeParagraph = () => {
+  paragraphWithId.innerText = "You clicked this button";
+};
+const divFunction = () => {
+  paragraphWithId.innerHTML = "You clicked a div";
+};
+clickADiv.addEventListener("click", divFunction);
+clickButton.addEventListener("click", changeParagraph);
+clickButton2.addEventListener("click", changeItBack);
