c3c69e5a579a4bca678c49660ade951d711ce4ee
michaeljgrant <mjg.grant.m@gmail.com>
2024-08-12 19:58:43 +1000

    change page to render home

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-const paragraphWithId = document.getElementById("first-paragraph");
-const clickButton = document.getElementById("first-button");
-const clickButton2 = document.getElementById("another-button");
-const clickADiv = document.getElementById("myClickableDiv");
+const header = document.getElementById("header");
+const navBar = document.getElementById("navigation");
+const contentSection = document.getElementById("content");
 
-const changeItBack = () => {
-  paragraphWithId.innerText =
-    "Tom's mother is a fine cook and hopefully she makes us a really nice pie one day";
+const renderHomePage = () => {
+  header.innerText = "This is our header";
+  navBar.innerHTML = "<a href='google.com'>Google</a>";
+  contentSection.innerHTML = "<p>This is our homepage content</p>";
 };
-const changeParagraph = () => {
-  paragraphWithId.innerText = "You clicked this button";
-};
-const divFunction = () => {
-  paragraphWithId.innerHTML = "You clicked a div";
-};
-clickADiv.addEventListener("click", divFunction);
-clickButton.addEventListener("click", changeParagraph);
-clickButton2.addEventListener("click", changeItBack);
+
+renderHomePage();
