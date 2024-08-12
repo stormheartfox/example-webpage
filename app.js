a1685be48200972ed5d3801a3b38940431ffa863
michaeljgrant <mjg.grant.m@gmail.com>
2024-08-12 20:50:27 +1000

    create semantic website example

--- app.js
+++ app.js
@@ -1,44 +1,37 @@
 const header = document.getElementById("header");
-const navBar = document.getElementById("navigation");
+const homeNav = document.getElementById("homeNav");
+const infoNav = document.getElementById("infoNav");
+const interestsNav = document.getElementById("interestsNav");
 const contentSection = document.getElementById("content");
-function ourWebsite() {
-  function navBarRender() {
-    navBar.innerHTML = (
-      <>
-        <a onClick={renderHomePage}>Home</a>
-        <a onClick={renderInfo}>Info</a>
-        <a onClick={renderInterests}>Interests</a>
-      </>
-    );
-  }
-  function renderHomePage() {
-    header.innerHTML = "<h1>This is our header</h1>";
-    navBarRender();
-    contentSection.innerHTML = "<p>This is our homepage content</p>";
-  }
-  function renderInfo() {
-    header.innerHTML = "<h1>Tom's Info Page</h1>";
-    navBarRender();
-    contentSection.innerHTML = (
-      <>
+const pageHeader = document.getElementById("pageHeader");
+const footer = document.getElementById("footer");
+
+homeNav.addEventListener("click", renderHomePage);
+infoNav.addEventListener("click", renderInfo);
+interestsNav.addEventListener("click", renderInterests);
+header.innerHTML = "<h1>Tom's Website</h1>";
+footer.innerHTML = "This page was made by Tom & Michael";
+function renderHomePage() {
+  contentSection.innerHTML = "<p>This is our homepage content</p>";
+  pageHeader.innerHTML = "<h2>Homepage Article Header</h2>";
+}
+function renderInfo() {
+  pageHeader.innerHTML = "<h2>Info Article Header</h2>";
+  contentSection.innerHTML = `
         <p>
           Famous quotes include "parmesan and mozzarella are good but they don't
           get anywhere near most kinds of French cheese."
         </p>
         <p>This is our second paragraph</p>
-      </>
-    );
-  }
-  function renderInterests() {
-    header.innerHTML = "Tom's Interests";
-    navBarRender();
-    contentSection.innerHTML = (
+      `;
+}
+function renderInterests() {
+  pageHeader.innerHTML = "<h2>Interests Article Header</h2>";
+  contentSection.innerHTML = `
       <p>
         Tom describes himself as a sporty person and enjoys football, climbing
         and cycling
       </p>
-    );
-  }
-  renderHomePage();
+    `;
 }
-ourWebsite();
+renderHomePage();
